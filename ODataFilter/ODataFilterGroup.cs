eb2a78b10b09b8f4285e97ee0906a7cad179aa5c
Марина Антонова <antonova-1999@mail.ru>
2020-07-09 14:22:29 +0400

    project

---
+++ ODataFilter/ODataFilterGroup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ODataFilter
+{
+    class ODataFilterGroup
+    {
+        public string[] Filter { get; set; }
+        public string Operator { get; set; }
+
+        public void GroupString(string expr)
+        {
+            if (expr.Contains("or"))
+            {
+                Filter = expr.Split(" or ");
+                Operator = "OR";
+            }
+            else if(expr.Contains("and"))
+            {
+                Filter = expr.Split(" and ");
+                Operator = "AND";
+            }
+            else
+            {
+                Filter = new string[] { expr };
+            }
+        }
+    }
+}
---
+++ ODataFilter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ODataFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ODataFilterGroup group = new ODataFilterGroup();
+
+            group.GroupString("Price eq 5 or Price eq 6 or Price eq 8");
+
+            Console.WriteLine("Filter:");
+            foreach(string filter in group.Filter)
+            {
+                Console.WriteLine("\t" + filter);
+            }
+            Console.WriteLine("Operator: " + group.Operator);
+
+            ODataFilterGroup group2 = new ODataFilterGroup();
+
+            Console.ReadKey();
+        }
+    }
+}
